89645f107dea4b88d6fe83f6fc5c7616b2ab235f
wdl-stark <psp2000wdl@sina.com>
2020-06-02 16:11:36 +0800

    test

--- creator.d.ts
+++ creator.d.ts
@@ -2,7 +2,7 @@
 /** !#en
 The main namespace of Cocos2d-JS, all engine core classes, functions, properties and constants are defined in this namespace.
 !#zh
-Cocos 引擎的主要命名空间，引擎代码中所有的类，函数，属性和常量都在这个命名空间中定义。 */
+Cocos 引擎的主要命名空间，引擎代码中所有的类，函数，属性和常量都在这个命名空间中定义。 */  
 declare namespace cc {	
 	/** The current version of Cocos2d being used.<br/>
 	Please DO NOT remove this String, it is an important flag for bug tracking.<br/>
